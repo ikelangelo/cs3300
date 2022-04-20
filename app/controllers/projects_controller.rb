839aa857b8ad7ba1bb36a82d5cd9349c70bf0f1e
root <ikelangelo@gmail.com>
2022-04-20 22:23:37 +0000

    heroku api keys in place circleci yml file altered

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,8 @@
 class ProjectsController < ApplicationController
-  
+  before_action :set_project, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, only: %i[new edit update create destroy]
 
-  before_action :set_project, only: %i[ show edit update destroy ]
+  
 
   
 
